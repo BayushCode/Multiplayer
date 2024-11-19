2dab2ef7b7780d1b0ba36a0da7a3d171fdc03ae5
BayushCode <hiayush21@gmail.com>
2024-11-19 15:19:32 +0000

    h

--- Public/Server/server.js
+++ Public/Server/server.js
@@ -1,16 +1,12 @@
-const path = require('path');
-const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
-const publicPath = path.join(__dirname, '/../Public');
-const port = process.env.PORT || 3000;
-let app = express();
-let server = http.createServer(app);
-let io = socketIO(server);
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
-app.use(express.static( path));
-
-server.listen(port, ()=> {
-    console.log(`Server is up on port ${port}.`)
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
