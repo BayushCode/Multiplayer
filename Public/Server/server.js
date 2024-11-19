c1bcbb32b9e3ec2eafa5a91655ea268e8a0a6a57
BayushCode <hiayush21@gmail.com>
2024-11-19 20:41:28 +0000

    h

--- Public/Server/server.js
+++ Public/Server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('node:path');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 server.listen(3000, () => {
