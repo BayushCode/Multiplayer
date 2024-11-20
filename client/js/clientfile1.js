01fe0cf240a7b3fbd1ad8a994547a15fcde92356
BayushCode <hiayush21@gmail.com>
2024-11-20 15:25:17 +0000

    x

--- client/js/clientfile1.js
+++ client/js/clientfile1.js
@@ -1,1 +1,7 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
+const socket = io();
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
